c994257236dfad451e662f1a6fc90ed977ad9ceb
eenie <472279981@qq.com>
2015-08-07 09:19:05 +0800

    Add waveupdate funtion

--- app/src/main/java/com/neusoft/sheng/event_food/MainActivity.java
+++ app/src/main/java/com/neusoft/sheng/event_food/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -40,6 +41,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -54,7 +57,7 @@ public class MainActivity extends ActionBarActivity {
     ArrayList<Shop_info> Shop_infos=new ArrayList<Shop_info>();
     ArrayList<Food_info>food_infos=new ArrayList<Food_info>();
 
-
+    WaveSwipeRefreshLayout mWaveSwipeRefreshLayout;
 
     ActionBarDrawerToggle mDrawerToggle;
 
@@ -100,6 +103,18 @@ public class MainActivity extends ActionBarActivity {
                 R.string.hello_world);
         mDrawerToggle.syncState();
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+
+
+        mWaveSwipeRefreshLayout = (WaveSwipeRefreshLayout) findViewById(R.id.main_swipe);
+        mWaveSwipeRefreshLayout.setOnRefreshListener(new WaveSwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Do work to refresh the list here.
+                new Task().execute();
+            }
+        });
 
 
         toolbar.setOnClickListener(new View.OnClickListener() {
@@ -312,4 +327,34 @@ public class MainActivity extends ActionBarActivity {
          }
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
+
+    private class Task extends AsyncTask<Void, Void, String[]> {
+
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return new String[0];
+        }
+
+        @Override protected void onPostExecute(String[] result) {
+            // Call setRefreshing(false) when the list has been refreshed.
+            mWaveSwipeRefreshLayout.setRefreshing(false);
+            super.onPostExecute(result);
+        }
+    }
+
 }
+
+
